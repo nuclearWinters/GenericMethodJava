fe4d5ea5148da3ebc63a55b89141bbe16a387149
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2019-11-21 18:48:42 -0500

    first

---
+++ src/main/java/com/mycompany/methodjava/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.methodjava;
+
+/**
+ *
+ * @author Armando
+ */
+public class Main {
+    public static void main(String[] args) {
+    Par myObj = new Par();
+    myObj.setPar1("John"); // Set the value of the name variable to "John"
+    System.out.println(myObj.getPar1());
+    myObj.setPar2(1); // Set the value of the name variable to "John"
+    System.out.println(myObj.getPar2());
+  }
+}
---
+++ src/main/java/com/mycompany/methodjava/Par.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.methodjava;
+
+/**
+ *
+ * @author Armando
+ */
+public class Par< F, S > {
+    private F par1;
+    // Getter
+    public F getPar1() {
+        return par1;
+    }
+    // Setter
+    public void setPar1(F par1) {
+        this.par1 = par1;
+    }
+    private S par2;
+    // Getter
+    public S getPar2() {
+        return par2;
+    }
+    // Setter
+    public void setPar2(S par2) {
+        this.par2 = par2;
+    }
+}
